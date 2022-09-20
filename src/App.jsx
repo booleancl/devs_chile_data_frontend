bbf2ee80b5ba5cddd0cea370e136703fb11834b8
Gonzalo Pincheira A <g.pincheira.a@gmail.com>
2022-09-20 15:13:24 -0300

    ci(deploy): adding deploy to github pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react'
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js'
 import { Pie } from 'react-chartjs-2'
 import { getData } from './services/getData'
+import RiseLoader from 'react-spinners/RiseLoader'
 
 import './App.css'
 
@@ -14,14 +15,18 @@ const options = {
     }
   }
 }
+const wait = (ms) => new Promise((resolve) => setTimeout(resolve, ms))
 
 function App() {
   const [ pieData, setPieData ] = useState(null)
+  const [loader, setLoader] = useState(true)
   const [ title, setTitle ] = useState('')
 
   const getChartData = async () => {
-    const { label, labels, data, backgroundColor } = await getData()
-
+    const questionIndex = 6
+    const { label, labels, data, backgroundColor } = await getData(8)
+    // await wait(2000)
+    
     setTitle(label)
 
     setPieData({
@@ -34,15 +39,18 @@ function App() {
         }
       ]
     })
+    setLoader(false)
   }
 
   useEffect(() => {
     getChartData()
   }, [])
 
+  // useEffect(() => , [pieData])
+
   return (
     <div className="App">
-      { pieData &&
+      { !loader &&
         <div className="card">
           <h1>{ title }</h1>
           <Pie
@@ -51,6 +59,7 @@ function App() {
           />
         </div>
       }
+      <RiseLoader loading={loader} />
     </div>
   )
 }
--- src/services/getData.js
+++ src/services/getData.js
@@ -1,29 +1,38 @@
 import axios from 'axios'
 import stringToColor from 'string-to-color'
 
-const getDevProfiles = (responses) => {
-  const questionIndex = 6
+const getDevProfiles = (responses, questionIndex) => {
   let label = ''
   let chartData = {}
 
   responses
     .forEach((responseData) => {
       const targetData = responseData.answers[questionIndex]
+      const actualAnswer = targetData.answer
 
       if (!label) {
         label = targetData.question
       }
 
-      for (let answer of targetData.answer) {
-
-        chartData = {
-          ...chartData,
-          [answer]: {
-            color: stringToColor(answer),
-            count: chartData[answer] ? chartData[answer].count + 1 : 1
+      if(Array.isArray(actualAnswer)) {
+        for (let answer of actualAnswer) {
+          chartData = {
+            ...chartData,
+            [answer]: {
+              color: stringToColor(answer),
+              count: chartData[answer] ? chartData[answer].count + 1 : 1
+            }
           }
         }
-      }
+      } else {
+        chartData = {
+          ...chartData,
+          [actualAnswer]: {
+            color: stringToColor(actualAnswer),
+            count: chartData[actualAnswer] ? chartData[actualAnswer].count + 1 : 1
+          }
+        }
+      }  
     })
 
   const labels = Object.keys(chartData)
@@ -33,11 +42,11 @@ const getDevProfiles = (responses) => {
   return { label, labels, backgroundColor, data }
 }
 
-export const getData = async () => {
+export const getData = async (questionIndex) => {
   try {
     const response = await axios.get('https://dev-chile-boolean-bff.onrender.com/answers')
     
-    return getDevProfiles(response.data)
+    return getDevProfiles(response.data, questionIndex)
   } catch (error) {
     console.log(error.message)
   }
